77373ed1257cc834cbea97b49939497bc8992b5d
pixelfehler1996 <checkercraft1996@gmail.com>
2016-09-08 16:15:41 +0200

    difficulty setting

--- app/src/main/java/com/example/laudien/tictactoe/SettingsActivity.java
+++ app/src/main/java/com/example/laudien/tictactoe/SettingsActivity.java
@@ -3,12 +3,16 @@ package com.example.laudien.tictactoe;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
     SeekBar timeSeekBar;
+    SeekBar difficultySeekBar;
     TextView timeTextView;
+    TextView difficultyTextView;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +20,14 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         timeSeekBar = (SeekBar)findViewById(R.id.timeSeekBar);
+        difficultySeekBar = (SeekBar)findViewById(R.id.difficultySeekBar);
         timeTextView = (TextView)findViewById(R.id.timeTextView);
+        difficultyTextView = (TextView)findViewById(R.id.difficultyTextView);
+        sharedPreferences = getSharedPreferences("com.example.laudien.tictactoe",0);
 
-        SharedPreferences savedTime = getSharedPreferences("savedTime",0);
-        timeSeekBar.setProgress(savedTime.getInt("savedTime",20));
-
+        // Initialize timeSeekBar
+        timeSeekBar.setProgress(sharedPreferences.getInt("savedTime",20));
         timeTextView.setText(Integer.toString(timeSeekBar.getProgress()));
-
         timeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
@@ -40,13 +45,48 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+
+        // Initialize difficultySeekBar
+        difficultySeekBar.setProgress(sharedPreferences.getInt("difficulty", 1));
+        difficultyTextView.setText(difficultyToString(difficultySeekBar.getProgress()));
+        difficultySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                difficultyTextView.setText(difficultyToString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    String difficultyToString(int difficulty){
+        String diffString = "";
+        switch (difficulty){
+            case 0:
+                diffString = getResources().getString(R.string.easy);
+                break;
+            case 1:
+                diffString = getResources().getString(R.string.medium);
+                break;
+            case 2:
+                diffString = getResources().getString(R.string.hard);
+        }
+        return diffString;
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-
-        SharedPreferences savedTime = getSharedPreferences("savedTime",0);
-        savedTime.edit().putInt("savedTime", timeSeekBar.getProgress()).apply();
+    protected void onPause() {
+        super.onPause();
+        sharedPreferences.edit()
+                .putInt("savedTime", timeSeekBar.getProgress())
+                .putInt("difficulty", difficultySeekBar.getProgress())
+                .apply();
     }
 }
