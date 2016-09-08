a30a7ac58bd578cba4c92e1d157ffcd16fcae490
pixelfehler1996 <checkercraft1996@gmail.com>
2016-09-08 14:36:23 +0200

    removed unnecessary imports

--- app/src/main/java/com/example/laudien/tictactoe/GameActivity.java
+++ app/src/main/java/com/example/laudien/tictactoe/GameActivity.java
@@ -1,6 +1,5 @@
 package com.example.laudien.tictactoe;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.CountDownTimer;
@@ -8,7 +7,6 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
