6942550375b6961c8006c717f610bbaccec82180
pixelfehler1996 <checkercraft1996@gmail.com>
2016-09-08 14:58:52 +0200

    added a menu

--- app/src/main/java/com/example/laudien/tictactoe/GameActivity.java
+++ app/src/main/java/com/example/laudien/tictactoe/GameActivity.java
@@ -6,6 +6,7 @@ import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
@@ -285,6 +286,12 @@ public class GameActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         timer.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     void showShip(){
--- app/src/main/java/com/example/laudien/tictactoe/startActivity.java
+++ app/src/main/java/com/example/laudien/tictactoe/startActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -78,5 +79,10 @@ public class StartActivity extends AppCompatActivity {
         editor.putInt("savedTime",seekBar.getProgress());
 
         editor.commit();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
