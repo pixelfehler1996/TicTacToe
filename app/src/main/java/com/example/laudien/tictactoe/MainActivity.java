e52644b4ecc5f12570c5db099c1b5defd718ef6b
pixelfehler1996 <checkercraft1996@gmail.com>
2016-09-29 12:12:04 +0200

    make the code shorter

--- app/src/main/java/com/example/laudien/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/laudien/tictactoe/MainActivity.java
@@ -1,19 +1,13 @@
 package com.example.laudien.tictactoe;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
