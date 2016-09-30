f74e720d299c0c742138419c406b355b6fdd4411
pixelfehler1996 <checkercraft1996@gmail.com>
2016-09-30 12:21:31 +0200

    removed unnecessary import

--- app/src/main/java/com/example/laudien/tictactoe/SettingsFragment.java
+++ app/src/main/java/com/example/laudien/tictactoe/SettingsFragment.java
@@ -1,6 +1,5 @@
 package com.example.laudien.tictactoe;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
